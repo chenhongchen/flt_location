c36e65d2a5b68124b8e64e637b3b5a9a3dbcdd0a
gaohaosk <gaohaosk>
2021-05-24 11:32:29 +0800

    增加接口只获取当前定位

--- lib/flt_location.dart
+++ lib/flt_location.dart
@@ -30,6 +30,28 @@ class FltLocation {
     return locations;
   }
 
+/*
+  * 获取当前位置
+  * return:
+  * 成功： {'value' : {'locations':location}
+  * 'location':{'name':'xx','countryCode':'xx','country':'xx',
+  *             'province':'xx','locality':'xx','subLocality':'xx','thoroughfare':'xx',
+  *             'subThoroughfare':'xx','coordinate':['longitude','latitude']}
+  * 失败：{'err':'xx'}
+  * */
+  static Future<Map> get getLocation async {
+    var locationAlways = await PermissionHandler()
+        .checkPermissionStatus(PermissionGroup.locationAlways);
+    var locationWhenInUse = await PermissionHandler()
+        .checkPermissionStatus(PermissionGroup.locationWhenInUse);
+    if (locationAlways != PermissionStatus.granted &&
+        locationWhenInUse != PermissionStatus.granted) {
+      return null;
+    }
+
+    Map locations = await _channel.invokeMethod('getLocation');
+    return locations;
+  }
   /*
   * 搜索周边位置
   * argsMap: 要搜索的字符串，格式{'key':'xxxx'}
